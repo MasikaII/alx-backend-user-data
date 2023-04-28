36f4ed7f79293b52236e5eaf83ffec8b0b661bc9
Stephy <atienostephanie040@gmail.com>
2023-04-28 14:20:19 +0000

    encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords
+"""
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashing passwords
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checking password validity
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
